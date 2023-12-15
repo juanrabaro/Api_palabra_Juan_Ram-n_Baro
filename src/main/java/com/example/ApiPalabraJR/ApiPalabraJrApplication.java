446a036fe54b7b3aef1bb3e3e23e9b775967949d
juanrabaro <jrbaro@iesrafaelalberti.es>
2023-12-15 17:12:28 +0100

    proyecto inicializado

---
+++ src/main/java/com/example/ApiPalabraJR/ApiPalabraJrApplication.java
@@ -0,0 +1,13 @@
+package com.example.ApiPalabraJR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPalabraJrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPalabraJrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ApiPalabraJR/ApiPalabraJrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ApiPalabraJR;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPalabraJrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
